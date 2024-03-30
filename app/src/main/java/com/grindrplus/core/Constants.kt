2c275f5d269fb8bdd8c2ca90b8772917681edef1
R0rt1z2 <me@r0rt1z2.com>
2024-03-30 20:01:09 +0100

    GrindrPlus: Bump version identifiers for 24.2.0

--- app/src/main/java/com/grindrplus/core/Constants.kt
+++ app/src/main/java/com/grindrplus/core/Constants.kt
@@ -5,7 +5,7 @@ import de.robv.android.xposed.XC_MethodReplacement.*
 
 object Constants {
     const val GRINDR_PKG = "com.grindrapp.android"
-    const val GRINDR_PKG_VERSION_NAME = "24.1.0"
+    const val GRINDR_PKG_VERSION_NAME = "24.2.0"
 
     const val NUM_OF_COLUMNS = 3
 
