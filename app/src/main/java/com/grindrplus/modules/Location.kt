502384c77a6994578ca8caf0dc8808b3ec483529
ortiz.leal.roger <ortiz.leal.roger@alumnat.copernic.cat>
2024-04-01 01:55:52 +0200

    GrindrPlus: Rework commands again

--- app/src/main/java/com/grindrplus/modules/Location.kt
+++ app/src/main/java/com/grindrplus/modules/Location.kt
@@ -1,21 +1,27 @@
 package com.grindrplus.modules
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.os.Build
+import android.widget.Toast
+import com.grindrplus.Hooker
 import com.grindrplus.Hooker.Companion.config
 import com.grindrplus.Hooker.Companion.sharedPref
 import com.grindrplus.core.Command
 import com.grindrplus.core.CommandModule
 import com.grindrplus.core.Utils.getLatLngFromLocationName
 import com.grindrplus.core.Utils.getLocationPreference
-import com.grindrplus.core.Utils.logChatMessage
 import com.grindrplus.core.Utils.setLocationPreference
+import com.grindrplus.core.Utils.showDialog
+import com.grindrplus.core.Utils.showToast
 import com.grindrplus.core.Utils.toggleSetting
 
 class Location(recipient: String, sender: String) : CommandModule(recipient, sender) {
     @Command(name = "teleport", aliases = ["tp"], help = "Teleport to a location.")
     fun teleport(args: List<String>) {
         if (args.isEmpty()) {
-            return logChatMessage(toggleSetting("teleport_enabled", "Teleport"),
-                recipient, sender)
+            return showToast(Toast.LENGTH_LONG,
+                toggleSetting("teleport_enabled", "Teleport"))
         }
 
         if (!config.readBoolean("teleport_enabled", false)) {
@@ -26,13 +32,13 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
             args.size == 1 && args[0].contains(",") -> {
                 val (lat, lon) = args[0].split(",").map { it.trim().toDouble() }
                 setLocationPreference("teleport_location", lat, lon)
-                logChatMessage("Teleported to $lat, $lon.", recipient, sender)
+                showToast(Toast.LENGTH_LONG, "Teleported to $lat, $lon.")
             }
             args.size == 2 && args.all { it.toDoubleOrNull() != null } -> {
                 val lat = args[0].toDouble()
                 val lon = args[1].toDouble()
                 setLocationPreference("teleport_location", lat, lon)
-                logChatMessage("Teleported to $lat, $lon.", recipient, sender)
+                showToast(Toast.LENGTH_LONG, "Teleported to $lat, $lon.")
             }
             else -> {
                 val aliases = config.readMap("teleport_aliases")
@@ -40,17 +46,14 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
                     val (lat, lon) = aliases.getString(args.joinToString(" "))
                         .split(",").map { it.trim().toDouble() }
                     setLocationPreference("teleport_location", lat, lon)
-                    logChatMessage("Teleported to $lat, $lon.", recipient, sender)
+                    showToast(Toast.LENGTH_LONG, "Teleported to $lat, $lon.")
                 } else {
                     val coordinates = getLatLngFromLocationName(args.joinToString(" "))
                     if (coordinates != null) {
                         setLocationPreference("teleport_location", coordinates.first, coordinates.second)
-                        logChatMessage(
-                            "Teleported to ${coordinates.first}, ${coordinates.second}.",
-                            recipient, sender
-                        )
+                        showToast(Toast.LENGTH_LONG, "Teleported to ${coordinates.first}, ${coordinates.second}.")
                     } else {
-                        logChatMessage("Could not find location.", recipient, sender)
+                        showToast(Toast.LENGTH_LONG, "Invalid location.")
                     }
                 }
             }
@@ -61,15 +64,23 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
     fun location(args: List<String>) {
         val location = getLocationPreference("teleport_location")
         if (!config.readBoolean("teleport_enabled", false)) {
-            logChatMessage("Teleport is disabled.", recipient, sender)
+            return showToast(Toast.LENGTH_LONG, "Teleport is disabled.")
         } else {
             if (location != null) {
-                logChatMessage(
+                showDialog("Current teleport location",
                     "Current teleport location: ${location.first}, ${location.second}.",
-                    recipient, sender
+                    "OK", {}, "Copy location",
+                    {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            val clipboard = Hooker.appContext.getSystemService(ClipboardManager::class.java)
+                            clipboard.setPrimaryClip(ClipData.newPlainText("Location",
+                                "${location.first}, ${location.second}"))
+                            showToast(Toast.LENGTH_LONG, "Location copied to clipboard.")
+                        }
+                    }
                 )
             } else {
-                logChatMessage("No teleport location set.", recipient, sender)
+                showToast(Toast.LENGTH_LONG, "No teleport location set.")
             }
         }
     }
@@ -81,9 +92,9 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
                 val location = sharedPref.getString("teleport_location", null)
                 if (location != null) {
                     config.addToMap("teleport_aliases", args[0], location)
-                    logChatMessage("Saved $location as ${args[0]}.", recipient, sender)
+                    showToast(Toast.LENGTH_LONG, "Saved $location as ${args[0]}.")
                 } else {
-                    logChatMessage("No teleport location set.", recipient, sender)
+                    showToast(Toast.LENGTH_LONG, "No teleport location set.")
                 }
             }
             2, 3 -> {
@@ -91,13 +102,13 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
                 val coordinates = coordinatesString.split(",").map { it.trim() }
                 if (coordinates.size == 2 && coordinates.all { it.toDoubleOrNull() != null }) {
                     config.addToMap("teleport_aliases", args[0], coordinates.joinToString(","))
-                    logChatMessage("Saved ${coordinates.joinToString(",")} as ${args[0]}.", recipient, sender)
+                    showToast(Toast.LENGTH_LONG, "Saved ${coordinates.joinToString(",")} as ${args[0]}.")
                 } else {
-                    logChatMessage("Invalid coordinates format. Use <lat,lon> or <lat> <lon>.", recipient, sender)
+                    showToast(Toast.LENGTH_LONG, "Invalid coordinates format. Use <lat,lon> or <lat> <lon>.")
                 }
             }
             else -> {
-                logChatMessage("Invalid command format. Use /save [alias] or /save [alias] [lat,lon].", recipient, sender)
+                showToast(Toast.LENGTH_LONG, "Invalid command format. Use /save [alias] or /save [alias] [lat,lon].")
             }
         }
     }
@@ -105,15 +116,22 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
     @Command(name = "delete", aliases = ["del"], help = "Delete a saved teleport location.")
     private fun delCommand(args: List<String>) {
         if (args.isEmpty()) {
-            logChatMessage("Please specify a teleport location alias.", recipient, sender)
+            showToast(Toast.LENGTH_LONG, "Please specify a teleport location alias.")
         } else {
             val teleportAliases = config.readMap("teleport_aliases")
             if (teleportAliases.has(args[0])) {
-                teleportAliases.remove(args[0])
-                config.writeConfig("teleport_aliases", teleportAliases)
-                logChatMessage("Deleted teleport location ${args[0]}.", recipient, sender)
+                showDialog("Delete teleport location",
+                    "Are you sure you want to delete the teleport location ${args[0]}?",
+                    "Yes",
+                    {
+                        teleportAliases.remove(args[0])
+                        config.writeConfig("teleport_aliases", teleportAliases)
+                        showToast(Toast.LENGTH_LONG, "Deleted teleport location ${args[0]}.")
+                    },
+                    "No", {}
+                )
             } else {
-                logChatMessage("No teleport location with alias ${args[0]} found.", recipient, sender)
+                showToast(Toast.LENGTH_LONG, "Teleport location ${args[0]} not found.")
             }
         }
     }
@@ -122,14 +140,23 @@ class Location(recipient: String, sender: String) : CommandModule(recipient, sen
     private fun listCommand(args: List<String>) {
         val teleportAliases = config.readMap("teleport_aliases")
         if (teleportAliases.length() == 0) {
-            logChatMessage("No teleport locations saved.", recipient, sender)
+            showToast(Toast.LENGTH_LONG, "No teleport locations saved.")
         } else {
             val teleportLocations = buildString {
                 teleportAliases.keys().forEach { alias ->
                     append("$alias: ${teleportAliases.getString(alias)}\n")
                 }
             }.trimEnd()
-            logChatMessage("Saved teleport locations:\n$teleportLocations", recipient, sender)
+            showDialog("Saved teleport locations", teleportLocations,
+                "OK", {}, "Copy locations",
+                {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        val clipboard = Hooker.appContext.getSystemService(ClipboardManager::class.java)
+                        clipboard.setPrimaryClip(ClipData.newPlainText("Locations", teleportLocations))
+                        showToast(Toast.LENGTH_LONG, "Locations copied to clipboard.")
+                    }
+                }
+            )
         }
     }
 }
