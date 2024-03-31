1f2d556b756e2e205e5c85673a6de920f9823b6e
ortiz.leal.roger <ortiz.leal.roger@alumnat.copernic.cat>
2024-03-31 19:57:37 +0200

    GrindrPlus: Add commands to migrate and clear saved phrases

--- app/src/main/java/com/grindrplus/modules/Settings.kt
+++ app/src/main/java/com/grindrplus/modules/Settings.kt
@@ -3,8 +3,10 @@ package com.grindrplus.modules
 import com.grindrplus.Hooker
 import com.grindrplus.core.Command
 import com.grindrplus.core.CommandModule
+import com.grindrplus.core.Logger
 import com.grindrplus.core.Utils.toggleSetting
 import com.grindrplus.core.Utils.logChatMessage
+import org.json.JSONObject
 
 class Settings(recipient: String, sender: String) : CommandModule(recipient, sender) {
     @Command(name = "redesign", aliases = ["rd"], help = "Toggle the new Grindr design.")
@@ -23,5 +25,45 @@ class Settings(recipient: String, sender: String) : CommandModule(recipient, sen
     private fun details(args: List<String>) {
         logChatMessage(toggleSetting("show_profile_details", "Profile details"),
             this.recipient, this.recipient)
+    }
+
+    @Command(name = "migrate", aliases = ["mg"], help = "Migrate old saved phrases to the new system.")
+    private fun migrate(args: List<String>) {
+        val oldSavedPhrasesIds = Hooker.sharedPref.getStringSet("phrases", emptySet()) ?: emptySet()
+        if (oldSavedPhrasesIds.isEmpty()) {
+            return logChatMessage("No old saved phrases found.", this.recipient, this.recipient)
+        }
+
+        val oldSavedPhrases = oldSavedPhrasesIds.associateWith {id ->
+            val text = Hooker.sharedPref.getString("phrase_${id}_text", "") ?: ""
+            val frequency = Hooker.sharedPref.getInt("phrase_${id}_frequency", 0)
+            val timestamp = Hooker.sharedPref.getLong("phrase_${id}_timestamp", 0L)
+            JSONObject().apply {
+                put("text", text)
+                put("frequency", frequency)
+                put("timestamp", timestamp)
+            }
+        }
+
+        val newPhrasesConfig = Hooker.config.readMap("phrases")
+        var newIdCounter = (newPhrasesConfig.keys().asSequence()
+            .mapNotNull { it.toIntOrNull() }
+            .maxOrNull() ?: 0) + 1
+        oldSavedPhrases.forEach { (_, oldPhrase) ->
+            val newId = newIdCounter++
+            newPhrasesConfig.put(newId.toString(), oldPhrase)
+        }
+
+        Hooker.config.writeConfig("phrases", newPhrasesConfig)
+        Hooker.config.writeConfig("id_counter", newIdCounter - 1)
+
+        logChatMessage("Migrated ${oldSavedPhrases.size} old saved phrases to the new system.",
+            this.recipient, this.recipient)
+    }
+
+    @Command(name = "rmphrases", aliases = ["rmph"], help = "Remove all saved phrases.")
+    private fun removePhrases(args: List<String>) {
+        Hooker.config.writeConfig("phrases", JSONObject())
+        logChatMessage("Removed all saved phrases.", this.recipient, this.recipient)
     }
 }
