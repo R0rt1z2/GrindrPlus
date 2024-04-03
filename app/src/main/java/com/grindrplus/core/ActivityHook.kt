d4743145b92aee445f714181a04d7ee632f07c19
ortiz.leal.roger <ortiz.leal.roger@alumnat.copernic.cat>
2024-04-03 03:22:27 +0200

    GrindrPlus: Fix crashes related to the current activity

--- app/src/main/java/com/grindrplus/core/ActivityHook.kt
+++ app/src/main/java/com/grindrplus/core/ActivityHook.kt
@@ -6,7 +6,14 @@ import de.robv.android.xposed.XC_MethodHook
 
 class ActivityHook : XC_MethodHook() {
     override fun afterHookedMethod(param: MethodHookParam) {
-        _currentActivity = param.result as Activity
+        val resultActivity = param.result as? Activity ?: return
+        val excludedActivities = setOf("ChatRoomPhotosActivity", "ProfilesActivity")
+
+        if (_currentActivity?.javaClass?.simpleName == "ChatActivityV2" &&
+            resultActivity.javaClass.simpleName in excludedActivities)
+            return
+
+        _currentActivity = resultActivity
     }
 
     companion object {
