45e2c340e0cf08c19bd937080e0192c6672f5eec
R0rt1z2 <me@r0rt1z2.com>
2024-11-13 23:59:59 +0100

    GrindrPlus: Nasty workaround for profiles

--- app/src/main/java/com/grindrplus/hooks/UnlimitedProfiles.kt
+++ app/src/main/java/com/grindrplus/hooks/UnlimitedProfiles.kt
@@ -1,8 +1,10 @@
 package com.grindrplus.hooks
 
+import com.grindrplus.core.Utils.openProfile
 import com.grindrplus.utils.Hook
 import com.grindrplus.utils.HookStage
 import com.grindrplus.utils.hook
+import de.robv.android.xposed.XposedHelpers.getObjectField
 
 class UnlimitedProfiles : Hook(
     "Unlimited profiles",
@@ -27,5 +29,15 @@ class UnlimitedProfiles : Hook(
             .hook("getUpsellType", HookStage.BEFORE) { param ->
                 param.setResult(null)
             }
+
+        // God forgive me for this abomination. This obviously breaks swiping between profiles
+        // so I have to figure out a way to properly handle this issue, since the experiment we
+        // were using back then (InaccessibleProfileManager) is no longer available.
+        findClass("com.grindrapp.android.ui.browse.B").hook("invokeSuspend", HookStage.BEFORE) { param ->
+            val cachedProfile = getObjectField(param.thisObject(), "j")
+            val profileId = getObjectField(cachedProfile, "profileIdLong")
+            openProfile(profileId.toString())
+            param.setResult(null)
+        }
     }
 }
