bf568f9ec3c46057cd4714693fdca74b9ec4a9cc
R0rt1z2 <me@r0rt1z2.com>
2025-01-03 15:00:40 +0100

    GrindrPlus: Dump available databases
    Change-Id: I57836fd39d70d1ae6cfeecae28d3ba3d5db24b17

--- app/src/main/java/com/grindrplus/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/grindrplus/ui/fragments/SettingsFragment.kt
@@ -278,6 +278,13 @@ class SettingsFragment : Fragment() {
             }
             appendLine("========================================")
         }
+        val databases = buildString {
+            val dbFiles = context.databaseList()
+            if (dbFiles.isNotEmpty()) {
+                dbFiles.forEach { dbFile -> appendLine(dbFile) }
+                appendLine("========================================")
+            }
+        }
 
         try {
             val childUri = DocumentsContract.buildDocumentUriUsingTree(
@@ -296,6 +303,7 @@ class SettingsFragment : Fragment() {
                 context.contentResolver.openOutputStream(newFileUri)?.use { outputStream ->
                     outputStream.write(info.toByteArray())
                     outputStream.write(activeHooks.toByteArray())
+                    outputStream.write(databases.toByteArray())
                     outputStream.write(logContent.toByteArray())
                     outputStream.flush()
                 }
