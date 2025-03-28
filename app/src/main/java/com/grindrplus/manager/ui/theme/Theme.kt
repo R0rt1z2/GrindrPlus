ccd08e032ace497a67f6bcd271759671350f5d2b
R0rt1z2 <me@r0rt1z2.com>
2025-03-28 20:12:49 +0100

    GrindrPlus: Fix ugly status/nav bar(s)
    Change-Id: Idb575215cafce3b3c4b8171b3f690a135641f03d

--- app/src/main/java/com/grindrplus/manager/ui/theme/Theme.kt
+++ app/src/main/java/com/grindrplus/manager/ui/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -111,18 +112,6 @@ fun GrindrPlusTheme(
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-
-            WindowCompat.getInsetsController(window, view).apply {
-                isAppearanceLightStatusBars = !darkTheme
-                isAppearanceLightNavigationBars = !darkTheme
-            }
-        }
     }
 
     MaterialTheme(
