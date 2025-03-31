f391ee154d2e2cc05bd559bfa27163cb93bcb47f
R0rt1z2 <me@r0rt1z2.com>
2025-04-01 00:21:32 +0200

    GrindrPlus: Simplify `OnlineIndicator`
    Change-Id: I4464b86ce2e2dab8448ed0390b1442d39ed09888

--- app/src/main/java/com/grindrplus/hooks/OnlineIndicator.kt
+++ app/src/main/java/com/grindrplus/hooks/OnlineIndicator.kt
@@ -1,5 +1,6 @@
 package com.grindrplus.hooks
 
+import com.grindrplus.GrindrPlus
 import com.grindrplus.core.Config
 import com.grindrplus.utils.Hook
 import com.grindrplus.utils.HookStage
@@ -10,13 +11,29 @@ class OnlineIndicator : Hook(
     "Online indicator",
     "Customize online indicator duration"
 ) {
-    val utils = "sd.f0" // search for ' <= 600000;'
+    private val utils = "sd.f0" // search for ' <= 600000;'
 
     override fun init() {
-        findClass(utils) // shouldShowOnlineIndicator()
-            .hook("b", HookStage.BEFORE) { param ->
-                val savedDuration = Config.get("online_indicator", 3).toString().toInt()
-                param.setResult(System.currentTimeMillis() - param.arg<Long>(0) <= savedDuration.minutes.inWholeMilliseconds)
+        try {
+            val utilsClass = findClass(utils)
+
+            val targetMethod = utilsClass.methods.firstOrNull { method ->
+                method.returnType == Boolean::class.java &&
+                        method.parameterTypes.size == 1 &&
+                        method.parameterTypes[0] == Long::class.java
             }
+
+            if (targetMethod == null) {
+                return GrindrPlus.logger.log("OnlineIndicator: Target method not found")
+            }
+
+            targetMethod.hook(HookStage.BEFORE) { param ->
+                val savedDuration = Config.get("online_indicator", 3).toString().toInt()
+                val durationMillis = savedDuration.minutes.inWholeMilliseconds
+                param.setResult(System.currentTimeMillis() - param.arg<Long>(0) <= durationMillis)
+            }
+        } catch (e: Exception) {
+            GrindrPlus.logger.log("OnlineIndicator: Unable to initialize hook: ${e.message}")
+        }
     }
 }
