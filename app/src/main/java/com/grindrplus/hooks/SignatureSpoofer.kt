fd3b919584cd05fe2fc0f152b1816df14f0d09c6
R0rt1z2 <me@r0rt1z2.com>
2025-04-01 16:12:35 +0200

    GrindrPlus: Spoof more Firebase APIs

--- app/src/main/java/com/grindrplus/hooks/SignatureSpoofer.kt
+++ app/src/main/java/com/grindrplus/hooks/SignatureSpoofer.kt
@@ -4,22 +4,33 @@ import com.grindrplus.utils.Hook
 import com.grindrplus.utils.HookStage
 import com.grindrplus.utils.hook
 
-class SignatureSpoofer : Hook(
-    "Signature Spoofer",
-    "Allow logging in with Google while using LSPatch"
-) {
-    @OptIn(ExperimentalStdlibApi::class)
-    private val packageSignature = "823f5a17c33b16b4775480b31607e7df35d67af8".hexToByteArray()
+class SignatureSpoofer :
+    Hook("Signature Spoofer", "Allow logging in with Google while using LSPatch") {
+    private val packageSignature = "823f5a17c33b16b4775480b31607e7df35d67af8"
     private val androidUtilsLight = "com.google.android.gms.common.util.AndroidUtilsLight"
-    private val getPackageCertificateHashBytes = "getPackageCertificateHashBytes"
+    private val firebaseInstallationServiceClient =
+        "com.google.firebase.installations.remote.FirebaseInstallationServiceClient"
+    private val configRealtimeHttpClient =
+        "com.google.firebase.remoteconfig.internal.ConfigRealtimeHttpClient"
+    private val configFetchHttpClient =
+        "com.google.firebase.remoteconfig.internal.ConfigFetchHttpClient"
 
+    @OptIn(ExperimentalStdlibApi::class)
     override fun init() {
+        findClass(androidUtilsLight).hook("getPackageCertificateHashBytes", HookStage.BEFORE) {
+                param ->
+            param.setResult(packageSignature.hexToByteArray())
+        }
 
-        // GMS Login
-        findClass(androidUtilsLight)
-            .hook(getPackageCertificateHashBytes, HookStage.BEFORE) {
-                    param ->
+        for (className in
+        listOf(
+            firebaseInstallationServiceClient,
+            configRealtimeHttpClient,
+            configFetchHttpClient
+        )) {
+            findClass(className).hook("getFingerprintHashForPackage", HookStage.BEFORE) { param ->
                 param.setResult(packageSignature)
             }
+        }
     }
 }
