708831bf1b72cd5d4683f003780f89d3c0ab7893
R0rt1z2 <me@r0rt1z2.com>
2025-04-19 18:25:23 +0200

    GrindrPlus: Show profile IDs in block list
    Change-Id: I34e328271881a8e3a552ee867f4055424f4687c8

--- app/src/main/java/com/grindrplus/hooks/ProfileDetails.kt
+++ app/src/main/java/com/grindrplus/hooks/ProfileDetails.kt
@@ -16,16 +16,17 @@ import com.grindrplus.ui.Utils.formatEpochSeconds
 import com.grindrplus.utils.Hook
 import com.grindrplus.utils.HookStage
 import com.grindrplus.utils.hook
+import com.grindrplus.utils.hookConstructor
 import de.robv.android.xposed.XposedHelpers.callMethod
 import de.robv.android.xposed.XposedHelpers.getObjectField
 import de.robv.android.xposed.XposedHelpers.setObjectField
+import java.util.ArrayList
 import kotlin.math.roundToInt
 
-class ProfileDetails : Hook(
-    "Profile details",
-    "Add extra fields and details to profiles"
-) {
+class ProfileDetails : Hook("Profile details", "Add extra fields and details to profiles") {
     private var boostedProfilesList = emptyList<String>()
+    private val blockedProfilesAdapter = "ed.A" // search for 'Intrinsics.checkNotNullParameter(individualUnblockActivityViewModel, "individualUnblockActivityViewModel");'
+    private val profileViewHolder = "ed.A\$b" // search for 'Intrinsics.checkNotNullParameter(individualUnblockActivityViewModel, "individualUnblockActivityViewModel");'
     private val distanceUtils = "com.grindrapp.android.utils.DistanceUtils"
     private val profileBarView = "com.grindrapp.android.ui.profileV2.ProfileBarView"
     private val profileViewState = "com.grindrapp.android.ui.profileV2.model.ProfileViewState"
@@ -33,85 +34,120 @@ class ProfileDetails : Hook(
         "com.grindrapp.android.persistence.model.serverdrivencascade.ServerDrivenCascadeCacheState"
     private val serverDrivenCascadeCachedProfile =
         "com.grindrapp.android.persistence.model.serverdrivencascade.ServerDrivenCascadeCachedProfile"
+    private var hasModifiedProfiles = false
 
     @SuppressLint("DefaultLocale")
     override fun init() {
-        findClass(serverDrivenCascadeCachedState)
-            .hook("getItems", HookStage.AFTER) { param ->
-                (param.getResult() as List<*>).filter {
-                    (it?.javaClass?.name ?: "") == serverDrivenCascadeCachedProfile
-                }.forEach {
+        findClass(serverDrivenCascadeCachedState).hook("getItems", HookStage.AFTER) { param ->
+            (param.getResult() as List<*>)
+                .filter { (it?.javaClass?.name ?: "") == serverDrivenCascadeCachedProfile }
+                .forEach {
                     if (getObjectField(it, "isBoosting") as Boolean) {
                         boostedProfilesList += callMethod(it, "getProfileId") as String
                     }
                 }
+        }
+
+        findClass(blockedProfilesAdapter).hook("onBindViewHolder", HookStage.BEFORE) { param ->
+            if (!hasModifiedProfiles) {
+                val profileList = getObjectField(param.thisObject(), "F") as ArrayList<*>
+                for (profile in profileList) {
+                    val profileId = callMethod(profile, "getProfileId") as String
+                    val displayName =
+                        (callMethod(profile, "getDisplayName") as? String)
+                            ?.takeIf { it.isNotEmpty() }
+                            ?.let { "$it ($profileId)" } ?: profileId
+                    setObjectField(profile, "displayName", displayName)
+                    hasModifiedProfiles = true
+                }
+            }
+        }
+
+        findClass(profileViewHolder).hookConstructor(HookStage.AFTER) { param ->
+            val textView =
+                getObjectField(param.thisObject(), "p") as TextView
+
+            textView.setOnLongClickListener {
+                val text = textView.text.toString()
+                val profileId = if ("(" in text && ")" in text)
+                    text.substringAfter("(").substringBefore(")")
+                else text
+
+                copyToClipboard("Profile ID", profileId)
+                GrindrPlus.showToast(Toast.LENGTH_LONG, "Profile ID: $profileId")
+                true
+            }
+        }
+
+        findClass(profileBarView).hook("setProfile", HookStage.BEFORE) { param ->
+            val profileId = getObjectField(param.arg(0), "profileId") as String
+            val accountCreationTime =
+                formatEpochSeconds(GrindrPlus.spline.invert(profileId.toDouble()).toLong())
+            val distance = callMethod(param.arg(0), "getDistance") ?: "Unknown (hidden)"
+            setObjectField(param.arg(0), "distance", distance)
+
+            if (profileId in boostedProfilesList) {
+                val lastSeen = callMethod(param.arg(0), "getLastSeenText")
+                setObjectField(param.arg(0), "lastSeenText", "$lastSeen (Boosting)")
             }
 
-        findClass(profileBarView)
-            .hook("setProfile", HookStage.BEFORE) { param ->
-                val profileId = getObjectField(param.arg(0), "profileId") as String
-                val accountCreationTime = formatEpochSeconds(GrindrPlus.spline.invert(profileId.toDouble()).toLong())
-                val distance = callMethod(param.arg(0), "getDistance") ?: "Unknown (hidden)"
-                setObjectField(param.arg(0), "distance", distance)
+            val displayName = callMethod(param.arg(0), "getDisplayName") ?: profileId
+            setObjectField(param.arg(0), "displayName", displayName)
 
-                if (profileId in boostedProfilesList) {
-                    val lastSeen = callMethod(param.arg(0), "getLastSeenText")
-                    setObjectField(param.arg(0), "lastSeenText", "$lastSeen (Boosting)")
-                }
+            val viewBinding = getObjectField(param.thisObject(), "c")
+            val displayNameTextView = getObjectField(viewBinding, "c") as TextView
 
-                val displayName = callMethod(param.arg(0), "getDisplayName") ?: profileId
-                setObjectField(param.arg(0), "displayName", displayName)
+            displayNameTextView.setOnLongClickListener {
+                GrindrPlus.showToast(Toast.LENGTH_LONG, "Profile ID: $profileId")
+                copyToClipboard("Profile ID", profileId)
+                true
+            }
 
-                val viewBinding = getObjectField(param.thisObject(), "c")
-                val displayNameTextView = getObjectField(viewBinding, "c") as TextView
-
-                displayNameTextView.setOnLongClickListener {
-                    GrindrPlus.showToast(Toast.LENGTH_LONG, "Profile ID: $profileId")
-                    copyToClipboard("Profile ID", profileId)
-                    true
-                }
-
-                displayNameTextView.setOnClickListener {
-                    val properties = mapOf(
+            displayNameTextView.setOnClickListener {
+                val properties =
+                    mapOf(
                         "Estimated creation" to accountCreationTime,
                         "Profile ID" to profileId,
-                        "Approximate distance" to Utils.safeGetField(
-                            param.arg(0),
-                            "approximateDistance"
-                        ) as? Boolean,
-                        "Found via teleport" to Utils.safeGetField(
-                            param.arg(0),
-                            "foundViaTeleport"
-                        ) as? Boolean,
-                        "Favorite" to Utils.safeGetField(param.arg(0), "isFavorite") as? Boolean,
-                        "From viewed me" to Utils.safeGetField(param.arg(0), "isFromViewedMe") as? Boolean,
-                        "Inaccessible profile" to Utils.safeGetField(
-                            param.arg(0),
-                            "isInaccessibleProfile"
-                        ) as? Boolean,
-                        "JWT boosting" to Utils.safeGetField(param.arg(0), "isJwtBoosting") as? Boolean,
+                        "Approximate distance" to
+                                Utils.safeGetField(param.arg(0), "approximateDistance") as? Boolean,
+                        "Found via teleport" to
+                                Utils.safeGetField(param.arg(0), "foundViaTeleport") as? Boolean,
+                        "Favorite" to
+                                Utils.safeGetField(param.arg(0), "isFavorite") as? Boolean,
+                        "From viewed me" to
+                                Utils.safeGetField(param.arg(0), "isFromViewedMe") as? Boolean,
+                        "Inaccessible profile" to
+                                Utils.safeGetField(param.arg(0), "isInaccessibleProfile")
+                                        as? Boolean,
+                        "JWT boosting" to
+                                Utils.safeGetField(param.arg(0), "isJwtBoosting") as? Boolean,
                         "New" to Utils.safeGetField(param.arg(0), "isNew") as? Boolean,
-                        "Teleporting" to Utils.safeGetField(param.arg(0), "isTeleporting") as? Boolean,
-                        "Online now" to Utils.safeGetField(param.arg(0), "onlineNow") as? Boolean
-                    ).filterValues { it != null }
+                        "Teleporting" to
+                                Utils.safeGetField(param.arg(0), "isTeleporting") as? Boolean,
+                        "Online now" to
+                                Utils.safeGetField(param.arg(0), "onlineNow") as? Boolean
+                    )
+                        .filterValues { it != null }
 
-                    val dialog = AlertDialog.Builder(it.context)
+                val dialog =
+                    AlertDialog.Builder(it.context)
                         .setTitle("Hidden profile details")
-                        .setMessage(properties.map { (key, value) -> "• $key: $value" }
-                            .joinToString("\n"))
+                        .setMessage(
+                            properties.map { (key, value) -> "• $key: $value" }.joinToString("\n")
+                        )
                         .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
                         .create()
 
-                    dialog.show()
-                }
+                dialog.show()
             }
+        }
 
-        findClass(distanceUtils)
-            .hook("c", HookStage.AFTER) { param ->
-                val distance = param.arg<Double>(1)
-                val isFeet = param.arg<Boolean>(2)
+        findClass(distanceUtils).hook("c", HookStage.AFTER) { param ->
+            val distance = param.arg<Double>(1)
+            val isFeet = param.arg<Boolean>(2)
 
-                param.setResult(if (isFeet) {
+            param.setResult(
+                if (isFeet) {
                     val feet = (distance * 3.280839895).roundToInt()
                     if (feet < 5280) {
                         String.format("%d feet", feet)
@@ -125,36 +161,38 @@ class ProfileDetails : Hook(
                     } else {
                         String.format("%d km %d m", meters / 1000, meters % 1000)
                     }
-                })
-            }
+                }
+            )
+        }
 
-        findClass(profileViewState)
-            .hook("getWeight", HookStage.AFTER) { param ->
-                val weight = param.getResult()
-                val height = callMethod(param.thisObject(), "getHeight")
+        findClass(profileViewState).hook("getWeight", HookStage.AFTER) { param ->
+            val weight = param.getResult()
+            val height = callMethod(param.thisObject(), "getHeight")
 
-                if (weight != null && height != null) {
-                    val BMI = calculateBMI(
+            if (weight != null && height != null) {
+                val BMI =
+                    calculateBMI(
                         "kg" in weight.toString(),
                         w2n("kg" in weight.toString(), weight.toString()),
                         h2n("kg" in weight.toString(), height.toString())
                     )
-                    if (Config.get("do_gui_safety_checks", true) as Boolean) {
-                        if (weight.toString().contains("(")) {
-                            Logger.w("BMI details are already present?")
-                            return@hook
-                        }
+                if (Config.get("do_gui_safety_checks", true) as Boolean) {
+                    if (weight.toString().contains("(")) {
+                        Logger.w("BMI details are already present?")
+                        return@hook
                     }
-                    param.setResult("$weight - ${String.format("%.1f", BMI)} (${
+                }
+                param.setResult(
+                    "$weight - ${String.format("%.1f", BMI)} (${
                         mapOf(
                             "Underweight" to 18.5,
                             "Normal weight" to 24.9,
                             "Overweight" to 29.9,
                             "Obese" to Double.MAX_VALUE
                         ).entries.first { it.value > BMI }.key
-                    })")
-                }
+                    })"
+                )
             }
-
+        }
     }
 }
