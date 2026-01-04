cc6402631d53c61da781f60825d3cd508a137f64
asdf biker <>
2026-01-04 15:03:24 +0100

    update online indicator to 25.20

--- app/src/main/java/com/grindrplus/hooks/OnlineIndicator.kt
+++ app/src/main/java/com/grindrplus/hooks/OnlineIndicator.kt
@@ -12,12 +12,33 @@ class OnlineIndicator : Hook(
     "Customize online indicator duration"
 ) {
     val utils = "Vm.m0" // search for ' <= 600000;'
+    val isFeatureFlagEnabled = "ih.e" // search for 'implements IsFeatureFlagEnabled'
 
     override fun init() {
-        findClass(utils) // shouldShowOnlineIndicator()
+        val savedDurationMinutes = Config.get("online_indicator", 3).toString().toInt()
+        val savedDurationMillis = savedDurationMinutes.minutes.inWholeMilliseconds
+
+        findClass(utils)// shouldShowOnlineIndicator()
             .hook("a", HookStage.BEFORE) { param ->
-                val savedDuration = Config.get("online_indicator", 3).toString().toInt()
-                param.setResult(System.currentTimeMillis() - param.arg<Long>(0) <= savedDuration.minutes.inWholeMilliseconds)
+                val lastSeen = param.arg<Long>(0)
+                param.setResult(System.currentTimeMillis() - lastSeen <= savedDurationMillis)
             }
+
+        findClass(isFeatureFlagEnabled)
+            .hook("a", HookStage.BEFORE) { param ->
+                val a = param.args()[0]
+                val flagKey = a!!.javaClass.getMethod("getKey").invoke(a)
+
+                if (flagKey == "online-until-updates")
+                    param.setResult(false)
+            }
+
+        findClass("com.grindrapp.android.utils.ProfileUtilsV2") // search for 'R.string.profile_time_online_minutes_ago'
+            .hook("b", HookStage.BEFORE) { param ->
+                val onlineUntilThreshold = param.arg<Long>(1)
+                if (onlineUntilThreshold == 600000L)
+                    param.setArg(1, savedDurationMillis)
+            }
+
     }
 }
