b3b8e61688c2e75b934b4c42c736a805ba7899af
david <david@git>
2026-01-04 04:35:58 +0100

    UnlockQuickbar

--- app/src/main/java/com/grindrplus/hooks/UnlockExplorer.kt
+++ app/src/main/java/com/grindrplus/hooks/UnlockExplorer.kt
@@ -10,21 +10,12 @@ class UnlockExplorer : Hook(
     "Unlock all profiles in Explorer"
 ) {
     override fun init() {
-        // *always* give you 5 *remaining* profiles to unlock in Explorer mode
-        // 5 is a nice low number that looks good in the UI
-
-        // HOW TO MAINTAIN: Hook both methods below, both exists in the smali and should probably be
-        // hooked, JADX JAVA simplifies and removes componentXX, look in JADX JAVA comment or search
-        // "numOfFreeExploreChatsRemaining" in smali to find all instances
-
         findClass("com.grindrapp.android.ui.profileV2.model.ProfileViewState")
-            .hook("getNumOfFreeExploreChatsRemaining", HookStage.BEFORE) { param ->
-                param.setResult(5)
-            }
-
-        findClass("com.grindrapp.android.ui.profileV2.model.ProfileViewState")
-            .hook("component58", HookStage.BEFORE) { param ->
-                param.setResult(5)
+            .hook("getShouldLockQuickbar", HookStage.BEFORE) { param ->
+                param.setResult(false)
             }
     }
+    // Alternative methods
+    // getNumOfFreeExploreChatsRemaining
+    // numOfFreeExploreChatsRemaining
 }
