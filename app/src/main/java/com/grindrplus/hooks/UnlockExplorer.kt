e9945d9ba27bf2c1548a245073b197549cf136a5
DavidL <166995747+KotlinNight@users.noreply.github.com>
2026-01-08 19:07:39 +0100

    GrindrPlus: Unlock all profiles in Explorer
    Co-authored-by: david <david@git>

---
+++ app/src/main/java/com/grindrplus/hooks/UnlockExplorer.kt
@@ -0,0 +1,21 @@
+package com.grindrplus.hooks
+
+import com.grindrplus.utils.Hook
+import com.grindrplus.utils.HookStage
+import com.grindrplus.utils.hook
+
+// supported version: 25.20.0
+class UnlockExplorer : Hook(
+    "Unlock Explorer",
+    "Unlock all profiles in Explorer"
+) {
+    override fun init() {
+        findClass("com.grindrapp.android.ui.profileV2.model.ProfileViewState")
+            .hook("getShouldLockQuickbar", HookStage.BEFORE) { param ->
+                param.setResult(false)
+            }
+    }
+    // Alternative methods
+    // getNumOfFreeExploreChatsRemaining
+    // numOfFreeExploreChatsRemaining
+}
