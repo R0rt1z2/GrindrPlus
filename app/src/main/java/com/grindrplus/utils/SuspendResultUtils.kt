26f72ba81f3c503614cd682c844c6fdb5a78e91c
gustarmartins <gufavmartins@hotmail.com>
2026-02-18 22:04:54 +0100

    fix withSuspendResult

--- app/src/main/java/com/grindrplus/utils/SuspendResultUtils.kt
+++ app/src/main/java/com/grindrplus/utils/SuspendResultUtils.kt
@@ -4,8 +4,10 @@ import de.robv.android.xposed.XC_MethodHook
 
 fun withSuspendResult(args: Array<Any?>, result: Any, onResult: (Array<Any?>, Any) -> Any): Any {
     return if (result.toString() == "COROUTINE_SUSPENDED") {
+        val continuation = args.last()!!
         var unhook: Set<XC_MethodHook.Unhook>? = null
-        unhook = args.last()!!.javaClass.hook("invokeSuspend", HookStage.BEFORE) {
+        unhook = continuation.javaClass.hook("invokeSuspend", HookStage.BEFORE) {
+            if (it.thisObject() !== continuation) return@hook // skip other instances
             unhook?.forEach(XC_MethodHook.Unhook::unhook)
             unhook = null
             it.setArg(0, onResult(args, it.arg(0)))
